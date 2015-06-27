cc275b208cc0e33bee0f8a2edd6c1e94d688837c
Max Medina <maxmedina05@gmail.com>
2015-06-27 10:51:28 -0400

    add player, scrolling effect, bolders falling

---
+++ src/Assets/scripts/ObstacleGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+    public GameObject prefab;
+    float speed = -3.0f;
+    GameObject ninjaObj;
+    public float minSpawnTime = 0.75f;
+    public float maxSpawnTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("SpawnObstacle", minSpawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnObstacle() {
+        // 1
+        Camera camera = Camera.main;
+        Vector3 cameraPos = camera.transform.position;
+        float xMax = camera.aspect * camera.orthographicSize;
+        float xRange = camera.aspect * camera.orthographicSize * 1.75f;
+        float yMax = camera.orthographicSize - 0.5f;
+        // 2
+        Vector3 catPos =
+          new Vector3(cameraPos.x + Random.Range(xMax - xRange, xMax),
+                      Random.Range(-yMax, yMax),
+                      prefab.transform.position.z);
+        Instantiate(prefab, catPos, Quaternion.identity);
+
+        Invoke("SpawnObstacle", Random.Range(minSpawnTime, maxSpawnTime));
+    }
+}
